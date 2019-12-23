bf3b1587c5b07cbff267f94fcdeb74fc4fe0e0fe
jcg678 <jcg678@hotmail.es>
2019-12-23 17:40:09 +0100

    Genarando modelos

---
+++ src/app/models/users.ts
@@ -0,0 +1,14 @@
+
+export class Users {
+  constructor(
+    public id: number,
+    public name: string,
+    public surname: string,
+    public rol: string,
+    public email: string,
+    public password: string,
+    public description: string,
+    public image: string
+  ) {
+  }
+}
