a2cfe6476ee8dda192473cb994ae76086e4749f5
jcg678 <jcg678@hotmail.es>
2019-12-27 17:25:36 +0100

    Generando componente register

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  public page_title:string;
+
+
+  constructor() {
+    this.page_title ='Registrate';
+  }
+
+  ngOnInit() {
+    console.log('componente de registro');
+  }
+
+}
