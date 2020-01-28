b4676b7c2209be735d8b2a30045662573f65dc2a
jcg678 <jcg678@hotmail.es>
2020-01-28 19:49:41 +0100

    Guardando ajustes usuario

--- src/app/components/user-edit/user-edit.component.ts
+++ src/app/components/user-edit/user-edit.component.ts
@@ -14,6 +14,7 @@ export class UserEditComponent implements OnInit {
   public user:User;
   public identity;
   public token;
+  public status;
 
   constructor(
     private _userService: UserService
@@ -22,10 +23,42 @@ export class UserEditComponent implements OnInit {
     this.user = new User(1, '','','ROLE_USER', '','','','');
     this.identity = this._userService.getIdentity();
     this.token = this._userService.getToken();
-    this.user = this.identity;
+    this.user = new User(this.identity.sub, this.identity.name,this.identity.surname,this.identity.role, this.identity.email,'',this.identity.description,this.identity.image);
   }
 
   ngOnInit() {
+  }
+
+  onSubmit(form){
+    this._userService.update(this.token,this.user).subscribe(
+      response=>{
+        console.log(response);
+        if(response){
+
+          if(response.changes.name){
+            this.user.name =  response.changes.name;
+          }
+          if(response.changes.surname){
+            this.user.surname =  response.changes.surname;
+          }
+          if(response.changes.dscription){
+            this.user.description =  response.changes.description;
+          }
+          if(response.changes.image){
+            this.user.image =  response.changes.image;
+          }
+
+
+          this.identity = this.user;
+          localStorage.setItem('identity',JSON.stringify(this.identity));
+        }else{
+          this.status = 'response';
+        }
+      },error => {
+        this.status = 'response';
+        console.log(<any>error);
+      }
+    );
   }
 
 }
