523ea5e7c10d90019d9e80f215899e153b80ec65
jcg678 <jcg678@hotmail.es>
2020-02-01 11:45:31 +0100

    creando categoria

--- src/app/components/category-new/category-new.component.ts
+++ src/app/components/category-new/category-new.component.ts
@@ -2,24 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import {Router,ActivatedRoute, Params} from '@angular/router';
 import {UserService} from '../../services/user.service';
 import {Category} from '../../models/category';
-
+import {CategoryService} from '../../services/category.service';
 
 @Component({
   selector: 'app-category-new',
   templateUrl: './category-new.component.html',
   styleUrls: ['./category-new.component.css'],
-  providers:[UserService]
+  providers:[UserService, CategoryService]
 })
 export class CategoryNewComponent implements OnInit {
   public page_title: string;
   public identity;
   public token;
   public category: Category;
+  public status;
 
   constructor(
     private _route: ActivatedRoute,
     private _router: Router,
-    private _userService: UserService
+    private _userService: UserService,
+    private _categoryService: CategoryService
   ) {
     this.page_title="Crear nueva categoria";
     this.identity = this._userService.getIdentity();
@@ -27,11 +29,26 @@ export class CategoryNewComponent implements OnInit {
     this.category = new Category(1,'');
   }
 
-  ngOnInit() {
+  ngOnInit(){
   }
 
   onSubmit(form){
-    console.log(this.category);
+    this._categoryService.create(this.token, this.category).subscribe(
+      response =>{
+        if(response.status = 'success'){
+          this.category = response.category;
+          this.status = 'success';
+
+          //this._router.navigate(['/inicio']);
+        }else{
+          this.status = 'error';
+        }
+      },
+      error=>{
+        this.status = 'error';
+        console.log(<any>error)
+      }
+    )
   }
 
 }
---
+++ src/app/services/category.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import  {Category} from '../models/category';
+import {global} from './global';
+
+@Injectable()
+export class CategoryService {
+  public url: string;
+
+
+  constructor(
+    public _http: HttpClient
+  ) {
+    this.url = global.url;
+  }
+
+  create(token, category):Observable<any>{
+    let json = JSON.stringify(category);
+    let params = "json="+json;
+
+    let headers = new HttpHeaders().set('Content-type', 'application/x-www-form-urlencoded').set('Authorization',token);
+    console.log(params);
+    console.log(headers);
+    return this._http.post(this.url+'category',params,{headers:headers});
+
+  }
+
+}
