3260de8c78c66ced00eaf0af07b7e603c7347cf5
jcg678 <jcg678@hotmail.es>
2020-02-14 18:19:25 +0100

    Filtrar botones por usuario edit/delete

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,23 +3,30 @@ import {Post} from '../../models/post';
 import {PostService} from '../../services/post.service';
 import { global} from '../../services/global';
 
+import {UserService} from '../../services/user.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  providers: [PostService]
+  providers: [PostService, UserService]
 })
 export class HomeComponent implements OnInit {
   public page_title: string;
   public url;
   public posts: Array<Post>;
+  public identity;
+  public token;
 
   constructor(
-    private _postService: PostService
+    private _postService: PostService,
+    private _userService: UserService
   ) {
     this.page_title = 'Inicio';
     this.url = global.url;
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
   }
 
   ngOnInit() {
