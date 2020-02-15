3f3af77087c84561cda04ecd65844c97ebf06577
jcg678 <jcg678@hotmail.es>
2020-02-15 11:05:03 +0100

    Obteniendo info del detalle

--- src/app/components/post-detail/post-detail.component.ts
+++ src/app/components/post-detail/post-detail.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+import {Post} from '../../models/post';
+import {PostService} from '../../services/post.service';
 
 @Component({
   selector: 'app-post-detail',
   templateUrl: './post-detail.component.html',
-  styleUrls: ['./post-detail.component.css']
+  styleUrls: ['./post-detail.component.css'],
+  providers: [PostService]
 })
 export class PostDetailComponent implements OnInit {
-
-  constructor() { }
+      public post:Post;
+  constructor(
+    private _postService: PostService,
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) { }
 
   ngOnInit() {
+    this.getPost();
+  }
+
+  getPost(){
+    this._route.params.subscribe(params=>{
+        let id = +params['id'];
+
+        this._postService.getPost(id).subscribe(
+          response =>{
+              if(response.status == 'success'){
+                this.post = response.post;
+                console.log(this.post);
+              }else {
+                this._router.navigate(['inicio']);
+              }
+          },error=>{
+            console.log(error);
+            this._router.navigate(['inicio']);
+          }
+        )
+      }
+    );
   }
 
 }
