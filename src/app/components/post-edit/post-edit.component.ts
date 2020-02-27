e52edbd135e62e30ac2a5c3b8a94cc8a6159221d
jcg678 <jcg678@hotmail.es>
2020-02-27 19:50:33 +0100

    mostrando pagina en edicion de post

--- src/app/components/post-edit/post-edit.component.ts
+++ src/app/components/post-edit/post-edit.component.ts
@@ -19,6 +19,7 @@ export class PostEditComponent implements OnInit {
   public categories;
   public status;
   public is_edit:boolean;
+  public url;
 
   public afuConfig = {
     multiple: false,
@@ -58,6 +59,7 @@ export class PostEditComponent implements OnInit {
     this.identity = this._userService.getIdentity();
     this.token = this._userService.getToken();
     this.is_edit=true;
+    this.url = global.url;
   }
 
   ngOnInit() {
